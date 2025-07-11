53db625541162a9ef3537fca1e481a85bafd8495
Guilherme Barros Zanella <zanella.guilherme501@gmail.com>
2025-07-11 20:46:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,29 @@ from tensorflow.keras.models import load_model  # Usando tf.keras
 from PIL import Image, ImageOps
 import numpy as np
 
-# Configuração do NumPy para evitar notação científica
 np.set_printoptions(suppress=True)
-
+#Carregando o modelo e retornando ele em uma variavel
 def load_keras_model(model_path):
-    """Carrega o modelo Keras"""
     return load_model(model_path, compile=False)
-
+#Carregando as classes do labels.txt
 def load_class_labels(labels_path):
-    """Carrega os rótulos das classes"""
     with open(labels_path, "r") as f:
         return f.readlines()
-
+#Preparando a imagem para o modelo poder classifica-la
 def preprocess_image(image_path, target_size=(224, 224)):
-    """Pré-processa a imagem para predição"""
     image = Image.open(image_path).convert("RGB")
     image = ImageOps.fit(image, target_size, Image.Resampling.LANCZOS)
     image_array = np.asarray(image, dtype=np.float32)
     normalized_image_array = (image_array / 127.5) - 1  # Normaliza para [-1, 1]
     return normalized_image_array
-
+#Esta funcao esta sendo ultilizada na interface
 def main(image_path):
-    MODEL_PATH = "keras_Model.h5"
-    LABELS_PATH = "labels.txt"
-    IMAGE_PATH = image_path  # Substitua pelo seu arquivo
 
     try:
-        model = load_keras_model(MODEL_PATH)
-        class_names = load_class_labels(LABELS_PATH)
+        model = load_keras_model("keras_Model.h5")
+        class_names = load_class_labels("labels.txt")
         
-        processed_image = preprocess_image(IMAGE_PATH)
+        processed_image = preprocess_image(image_path)
         input_data = np.expand_dims(processed_image, axis=0)
         
         prediction = model.predict(input_data)
